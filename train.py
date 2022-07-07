378ce3328977664e5eeff105eb348c2b795f443b
simonblaue <sblaue@gmx.de>
2022-07-07 13:37:34 +0200

    added requirements

--- train.py
+++ train.py
@@ -5,6 +5,12 @@ from yolact_pkg.yolact import Yolact
 from yolact_pkg.train import train
 from settings import *
 
+## IMPORTATNT !! ##
+
+#   To train yolact you need cuda and pytorch installed
+# 
+# 
+
 if __name__ == '__main__':
     
     print("dir()", dir())
